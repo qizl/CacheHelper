6a813c8ebe2b4eefc703abf80f4de19ef172c365
qizl <qizl@guanhuai365.com>
2016-04-17 12:49:09 +0800

    多客户端测试

---
+++ src/CacheHelper.WinformClient/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Com.EnjoyCodes.CacheHelper.WinformClient
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            string key = "a";
+            object value = 123;
+            CacheHelper.Insert(key, value);
+            var result = CacheHelper.Get(key);
+        }
+    }
+}
